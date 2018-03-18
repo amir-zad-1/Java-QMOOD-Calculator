36a1b551574c8cc5edb7c24ad6b32086910cf09e
mahsaghoreishi <m.ghoreishi1@gmail.com>
2018-03-18 14:51:41 -0400

    Update CIS.java

--- src/metrics/CIS.java
+++ src/metrics/CIS.java
@@ -1,8 +1,10 @@
 package metrics;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import ast.ClassObject;
 import ast.SystemObject;
@@ -11,10 +13,12 @@ import ast.MethodObject;
 public class CIS 
 {
 	//mahsa ghoreishi
-
+	private Map<String, Integer> classMap = new HashMap<String, Integer>();
 	public double systemValue=0;
+	
 	public CIS(SystemObject system) 
 	{
+		int classesValue=0;
 		//the number of public methods in all classes
 		
 		List<MethodObject> arrMethods = new ArrayList();
@@ -27,7 +31,7 @@ public class CIS
 		while(classesItrator.hasNext())
 		{
 			ClassObject classObject = classesItrator.next();
-			 
+			classesValue = 0; 
 				
 			//get methods of the class
 			arrMethods = classObject.getMethodList(); 
@@ -40,15 +44,28 @@ public class CIS
 				if(modifier.equals("public"))
 				{
 					systemValue +=1;
+					classesValue += 1; 
 				}
 			
 			}
+			classMap.put(classObject.getName(), classesValue);
 			
 		}
 		systemValue = systemValue/setClasses.size();
 	}
 	
 
+	@Override
+	public String toString() 
+	{
+		StringBuilder sb = new StringBuilder();
+		for(String key : classMap.keySet()) 
+		{
+			sb.append(key).append("\t").append(classMap.get(key)).append("\n");
+		}
+		return sb.toString();
+	}
+	
 	public String toString2() {
 		return " " + systemValue;
 	}
