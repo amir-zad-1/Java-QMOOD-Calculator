68c37d1feaae3bdaf7c8a73af01d38038e52bb05
somayehhajabedi <31833237+somayehhajabedi@users.noreply.github.com>
2018-03-18 16:06:30 -0400

    Update DCC.java

--- src/metrics/DCC.java
+++ src/metrics/DCC.java
@@ -1,7 +1,9 @@
 package metrics;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import ast.ClassObject;
@@ -13,6 +15,7 @@ import ast.TypeObject;
 
 public class DCC { 
 	private List<String> ClassesInPackage = new ArrayList<String>();
+	private Map<String, Integer> classMap = new HashMap<String, Integer>();//SMYH
 	public double dccValue;
 	
 	public DCC(SystemObject system) {
@@ -24,6 +27,7 @@ public class DCC {
 			int IMCoupling = ExportCouplingCalculation(system, classObj);
 			
 			dccValue += IMCoupling;
+			classMap.put(classObj.getName(), IMCoupling);//SMYH
 		}
 		dccValue = dccValue / classes.size();
 	}
@@ -36,29 +40,48 @@ public class DCC {
 		int value = 0;
 		for(ClassObject eachClass : ObjectsInPackage) 
 		{
+			System.out.println("class :" + eachClass);
 			List<FieldObject> classFields = eachClass.getFieldList();
 			for(int i=0; i<classFields.size(); i++)
+			
 			{
 				TypeObject to =	classFields.get(i).getType();
 				if(ClassesInPackage.contains(to.toString()))
 				{
 					if(to.toString().equals(classObj.getName()))
+					
 					{
-						value++; 
+						value++;
+						
+						System.out.println("value1 :" + value);
+						
+						
 						break;
 					}
+					System.out.println("value2 :" + value);
 				}
 			}
+			classMap.put(classObj.getName(), value);//SMYH
 		}
+		
 		return value;
 	}
+	
 	
 	
 	
 	@Override
 	public String toString() 
 	{
-		return " " + dccValue;
+		return "System_Value: "+dccValue;
+	}
+	public String toString2() //SMYH
+	{
+		StringBuilder sb = new StringBuilder();
+		for (String key : classMap.keySet()) {
+			sb.append(key).append("\t").append(classMap.get(key)).append("\n");
+		}
+		return sb.toString();
 	}
 	
 }
