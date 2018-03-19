aedaeb195e42eab3f1b9987fec5260f585277e44
amir <amri.hosseinzadeh@c2ro.com>
2018-03-19 11:11:04 -0400

    Sample test data added

---
+++ src/tests/data/Animal.java
@@ -0,0 +1,23 @@
+package tests.data;
+
+public class Animal {
+	
+	public int age;
+	public int legs;
+	
+	public Animal()
+	{
+		
+	}
+	
+	public void makeSound()
+	{
+		
+	}
+	
+	public void walk()
+	{
+		
+	}
+	
+}
---
+++ src/tests/data/Lion.java
@@ -0,0 +1,22 @@
+package tests.data;
+
+public class Lion extends Mammal {
+	
+	public Lion()
+	{
+		
+	}
+	
+	@Override
+	public void makeSound() {
+		// TODO Auto-generated method stub
+		super.makeSound();
+	}
+	
+	@Override
+	public void walk() {
+		// TODO Auto-generated method stub
+		super.walk();
+	}
+
+}
